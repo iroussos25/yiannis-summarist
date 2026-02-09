6d01053e54a78549cd2b0ff9b23745a3f316559b
Yiannis <ioannis.roussos@hotmail.com>
2026-02-09 22:21:33 +0200

    update

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,4 +1,6 @@
 "use client"; 
+export const dynamic = "force-dynamic";
+
 import  { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js"; 
