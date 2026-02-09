cb3aa7d57530a3061829b65b6fefd35e8383a6c7
Yiannis <ioannis.roussos@hotmail.com>
2026-02-09 23:59:20 +0200

    updated

--- src/app/checkout/separate.tsx
+++ src/app/checkout/separate.tsx
@@ -4,10 +4,15 @@ import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "@/components/checkoutForm";
 import { useAppSelector } from '@/app/redux/hooks';
+import dynamic from "next/dynamic";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+const CheckoutForm = dynamic(
+    () => import("@/components/checkoutForm"),
+    { ssr: false }
+  );
+
 
 export default function CheckoutClient() {
   const [clientSecret, setClientSecret] = useState("");
